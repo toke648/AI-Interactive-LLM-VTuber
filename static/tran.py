4241129c266f0060ca57502d97120bba12faff23
toke648 <tim16673802@gmail.com>
2025-04-21 21:09:11 +0800

    fixed

--- static/tran.py
+++
@@ -1,9 +0,0 @@
-import edge_tts
-import asyncio
-
-async def generate_options():
-    voices = await edge_tts.list_voices()
-    for voice in voices:
-        print(f'<option value="{voice["ShortName"]}">{voice["ShortName"]}</option>')
-
-asyncio.run(generate_options())
